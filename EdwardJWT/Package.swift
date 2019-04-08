067a19443907b57d69707050a1af3693f27db323
edward <cuikefan@bituniverse.org>
2019-04-08 21:32:42 +0800

    update

--- EdwardJWT/Package.swift
+++ EdwardJWT/Package.swift
@@ -1,17 +1,1 @@
 // swift-tools-version:4.0
-
-import PackageDescriptio
-
-let package = Package(
-  name: "JWT",
-  products: [
-    .library(name: "JWT", targets: ["JWT"]),
-  ],
-  dependencies: dependencies,
-  targets: [
-    .target(name: "JWA", dependencies: targetDependencies, exclude: excludes),
-    .target(name: "JWT", dependencies: ["JWA"]),
-    .testTarget(name: "JWATests", dependencies: ["JWA"]),
-    .testTarget(name: "JWTTests", dependencies: ["JWT"]),
-  ]
-)
