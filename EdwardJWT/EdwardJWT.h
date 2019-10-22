23aa66f36a154c95962973ce974601447b869ea7
Rool Paap <rool@rool.nl>
2019-10-22 09:52:50 +0200

    Added Carthage support

---
+++ EdwardJWT/EdwardJWT.h
@@ -0,0 +1,19 @@
+//
+//  EdwardJWT.h
+//  EdwardJWT
+//
+//  Created by Rool Paap on 22/10/2019.
+//  Copyright © 2019 Rool Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EdwardJWT.
+FOUNDATION_EXPORT double EdwardJWTVersionNumber;
+
+//! Project version string for EdwardJWT.
+FOUNDATION_EXPORT const unsigned char EdwardJWTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EdwardJWT/PublicHeader.h>
+
+
